fd55d9165bbfe95d5081c5952ca7559b38d83a1f
saroshrd <saroshrd@hotmail.com>
2018-10-22 03:18:44 +0530

    Add files via upload

---
+++ Tables.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    
+    class BPMNNodeData
+    {
+        public string category { get; set; }
+        public string UID { get; set; }
+        public string incoming { get; set; }
+        public string outgoing { get; set; }
+
+
+        public static List<BPMNNodeData> lstBPMNNodeData = new List<BPMNNodeData>();
+        public static List<BPMNNodeData> GetBPMNNodeData(string category,string UID,string incoming, string outgoing)
+        {
+            lstBPMNNodeData.Add(new BPMNNodeData(){
+                                category =category,
+                                UID=UID,
+                                incoming=incoming,
+                                outgoing=outgoing});
+
+            return lstBPMNNodeData;
+
+        }
+
+
+    }
+}
